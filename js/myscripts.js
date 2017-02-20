0784ebd080b70d41256b57685e398f59ee12361b
Nathalie Marquesin <nmarquesin@live.com>
2017-02-20 14:34:26 +0000

    Add button dot functionality

--- js/myscripts.js
+++ js/myscripts.js
@@ -18,126 +18,136 @@ $(document).ready(function() {
     console.log(display);
   });
 
-  // functionality of button "2"
-$("#btn2").click(function() {
+    // functionality of button "2"
+  $("#btn2").click(function() {
+    if (display === "0")
+    {
+      display ="2";
+    }
+    else
+    {
+      display +="2";
+    }
+    $("#calc-display").html(display);
+    console.log(display);
+  });
+
+    // functionality of button "3"
+  $("#btn3").click(function() {
   if (display === "0")
   {
-    display ="2";
+    display ="3";
   }
   else
   {
-    display +="2";
+    display +="3";
   }
   $("#calc-display").html(display);
   console.log(display);
-});
+  });
 
-// functionality of button "3"
-$("#btn3").click(function() {
-if (display === "0")
-{
-  display ="3";
-}
-else
-{
-  display +="3";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "4"
+  $("#btn4").click(function() {
+  if (display === "0")
+  {
+    display ="4";
+  }
+  else
+  {
+    display +="4";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "4"
-$("#btn4").click(function() {
-if (display === "0")
-{
-  display ="4";
-}
-else
-{
-  display +="4";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "5"
+  $("#btn5").click(function() {
+  if (display === "0")
+  {
+    display ="5";
+  }
+  else
+  {
+    display +="5";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "5"
-$("#btn5").click(function() {
-if (display === "0")
-{
-  display ="5";
-}
-else
-{
-  display +="5";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "6"
+  $("#btn6").click(function() {
+  if (display === "0")
+  {
+    display ="6";
+  }
+  else
+  {
+    display +="6";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "6"
-$("#btn6").click(function() {
-if (display === "0")
-{
-  display ="6";
-}
-else
-{
-  display +="6";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "7"
+  $("#btn7").click(function() {
+  if (display === "0")
+  {
+    display ="7";
+  }
+  else
+  {
+    display +="7";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "7"
-$("#btn7").click(function() {
-if (display === "0")
-{
-  display ="7";
-}
-else
-{
-  display +="7";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "8"
+  $("#btn8").click(function() {
+  if (display === "0")
+  {
+    display ="8";
+  }
+  else
+  {
+    display +="8";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "8"
-$("#btn8").click(function() {
-if (display === "0")
-{
-  display ="8";
-}
-else
-{
-  display +="8";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "9"
+  $("#btn9").click(function() {
+  if (display === "0")
+  {
+    display ="9";
+  }
+  else
+  {
+    display +="9";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "9"
-$("#btn9").click(function() {
-if (display === "0")
-{
-  display ="9";
-}
-else
-{
-  display +="9";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "0"
+  $("#btn0").click(function() {
+  if (display !== "0")
+  {
+    display +="0";
+  }
+  $("#calc-display").html(display);
+  console.log(display);
+  });
 
-// functionality of button "0"
-$("#btn0").click(function() {
-if (display !== "0")
-{
-  display +="0";
-}
-$("#calc-display").html(display);
-console.log(display);
-});
+    // functionality of button "."
+  $("#btnDot").click(function() {
+    if (display.includes(".") === false)
+    {
+      display +=".";
+      $("#calc-display").html(display);
+      console.log(display);
+    }
+  });
 
 });
